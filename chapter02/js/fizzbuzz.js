db491e7f8fdccb1d1c993a81fc4bbc2be4ec3123
Byron6 <byrshaw@gmail.com>
2015-03-18 16:22:57 -0500

    all done

--- chapter02/js/fizzbuzz.js
+++ chapter02/js/fizzbuzz.js
@@ -1,4 +1,13 @@
 // Student Workspace
 Fizz.buzz = function () {
+	for(i = 0; i <= 100; i++) {
+    if(i % 3 === 0) {
+        console.log("frizz");
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    } else {
+        console.log(i);
+    };
+};
 
 };
