c926ee28b963f87ad90f111e882d30c05f92726f
Byron6 <byrshaw@gmail.com>
2015-03-19 17:34:31 -0500

    fixed it

--- chapter02/js/fizzbuzz.js
+++ chapter02/js/fizzbuzz.js
@@ -1,11 +1,18 @@
 // Student Workspace
 Fizz.buzz = function () {
+	
 	for(i = 0; i <= 100; i++) {
-    if(i % 3 === 0) {
-        console.log("frizz");
-    } else if (i % 5 === 0) {
+		
+		if (i % 5 === 0) {
+        
         console.log("buzz");
+    
+    } else if(i % 3 === 0) {
+        
+        console.log("fizz");
+  
     } else {
+        
         console.log(i);
     };
 };
